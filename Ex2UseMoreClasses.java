75d300252f81cebcca0c1f1a9d41f9acd8b80145
Albin1118 <31730135+Albin1118@users.noreply.github.com>
2017-10-06 11:36:56 +0200

    Add files via upload

--- Ex2UseMoreClasses.java
+++ Ex2UseMoreClasses.java
@@ -18,15 +18,20 @@ public class Ex2UseMoreClasses {
     void program() {
 
         // All true
+
         Point p = new Point(1, 2, 3);
-        out.println(p.one==1 && p.two==2 &&p.three==3);
+        out.println(p.x==1 && p.y==2 && p.z==3);
+        Point p2 = new Point(1, 2, 2);
+        out.println(p.distance(p2)==1);
         out.println(p.distance(new Point(p)) == 0);
         out.println(new Point(0, 0, 0).distance(new Point(1, 0, 0)) == 1);
 
         // Triangle uses Points!
-        /*Triangle t = new Triangle(new Point(0, 0, 0), new Point(0, 1, 0), new Point(1, 0, 0));
+
+        Triangle t = new Triangle(new Point(0, 0, 0), new Point(0, 1, 0), new Point(1, 0, 0));
+        out.println(t.pointA.x==0 && t.pointB.y==1 && t.pointC.z==0);
         out.println(abs(t.area() - 0.5) < 0.000001);
-        */
+        //*/
     }
 
     // ---------- Write your classes below this line ----------------------------
@@ -34,28 +39,28 @@ public class Ex2UseMoreClasses {
     // A class for points in 3D
 
     public class Point{
-        int one;
-        int two;
-        int three;
+        int x;
+        int y;
+        int z;
 
         Point(int first, int second, int third){
-            one=first;
-            two=second;
-            three=third;
+            x=first;
+            y=second;
+            z=third;
         }
         Point(Point p){
-            one=p.one;
-            two=p.two;
-            three=p.three;
+            x=p.x;
+            y=p.y;
+            z=p.z;
         }
-        public int distance(Point other){
-            double x_sub =((other.one - this.one));
+        public double distance(Point other){
+            double x_sub =((other.x - this.x));
             int distance_x = (int)Math.pow(x_sub, 2);
-            double y_sub = (other.two - this.two);
+            double y_sub = (other.y - this.y);
             int distance_y =(int)Math.pow(y_sub, 2);
-            double z_sub = (other.three - this.three);
+            double z_sub = (other.z - this.z);
             int distance_z = (int)Math.pow(z_sub, 2);
-            int result = (int)Math.round(Math.sqrt(distance_x + distance_y + distance_z));
+            double result = Math.sqrt(distance_x + distance_y + distance_z);
             return result;
         }
 
@@ -64,24 +69,29 @@ public class Ex2UseMoreClasses {
     // A class for a Triangle in 3D
 
     public class Triangle{
-        Point point1;
-        Point point2;
-        Point point3;
+        Point pointA;
+        Point pointB;
+        Point pointC;
 
         Triangle(Point p1, Point p2, Point p3){
-            point1.one = p1.one;
-            point1.two = p1.two;
-            point1.three = p1.three;
-
-            point2.one = p2.one;
-            point2.two = p2.two;
-            point2.three = p2.three;
-
-            point3.one = p3.one;
-            point3.two = p3.two;
-            point3.three = p3.three;
-
+            pointA = p1;
+            pointB = p2;
+            pointC = p3;
         }
+        public double area(){
+
+            double distanceA = pointA.distance(pointB);
+            double distanceB = pointB.distance(pointC);
+            double distanceC = pointC.distance(pointA);
+            int div = 2;
+            double semiperimeter = ((double)(distanceA +distanceB + distanceC)/div);
+
+            double areaABC =Math.sqrt(semiperimeter * (semiperimeter - distanceA) * (semiperimeter - distanceB) * (semiperimeter - distanceC));
+
+            return areaABC;
+        }
+
+
 
 
 
